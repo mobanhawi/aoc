058a62248d82e16dfc801d2aef93c18c076b03d9
Elbanhawi, Mohamed <mohamed.elbanhawi@anz.com>
2025-12-05 14:35:20 +1100

    bruteforce day2

--- 2025/2025.go
+++ 2025/2025.go
@@ -1,7 +1,11 @@
 package _025
 
-import "github.com/mobanhawi/aoc/2025/day1"
+import (
+	"github.com/mobanhawi/aoc/2025/day1"
+	"github.com/mobanhawi/aoc/2025/day2"
+)
 
 func Solve() {
 	day1.Solve()
+	day2.Solve()
 }
---
+++ 2025/day2/day2.go
@@ -0,0 +1,65 @@
+package day2
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func hasRepeatingPattern(s string) bool {
+	n := len(s)
+	// Try all possible pattern lengths from 1 to n/2
+	for patternLen := 1; patternLen <= n/2; patternLen++ {
+		if n%patternLen == 0 {
+			pattern := s[:patternLen]
+			matched := true
+			// Check if all chunks match the first pattern
+			for i := patternLen; i < n; i += patternLen {
+				if s[i:i+patternLen] != pattern {
+					matched = false
+					break
+				}
+			}
+			if matched {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+/*
+Solve https://adventofcode.com/2025/day/2
+*/
+func Solve() {
+	input, err := os.ReadFile("2025/day2/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(input)), ",")
+	fmt.Println("2025/day/2", solve(lines))
+}
+
+func solve(lines []string) int {
+	sum := 0
+
+	for _, line := range lines {
+		numbers := strings.Split(line, "-")
+		if len(numbers) != 2 {
+			log.Fatalf("invalid instruction: %s", line)
+		}
+		start, _ := strconv.Atoi(numbers[0])
+		end, _ := strconv.Atoi(numbers[1])
+		for i := start; i <= end; i++ {
+			stringNumber := strconv.Itoa(i)
+			if hasRepeatingPattern(stringNumber) {
+				sum += i
+			}
+		}
+	}
+
+	return sum
+}
