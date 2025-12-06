3de20a212a147ed683b184c8add7132596dd4f5d
Mohamed Elbanhawi <mohamed.elbanhawi@anz.com>
2025-12-06 14:36:28 +1100

    implement solutions for day 3 part 1 and part 2, updating output formatting

--- 2025/day3/day3.go
+++ 2025/day3/day3.go
@@ -2,6 +2,7 @@ package day3
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	"strconv"
@@ -18,7 +19,8 @@ func Solve() {
 	}
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	fmt.Println("2025/day/3", solve(lines))
+	fmt.Println("2025/day/3 part1", solvePt1(lines))
+	fmt.Println("2025/day/3 part2", solvePt2(lines))
 }
 
 type Node struct {
@@ -26,7 +28,51 @@ type Node struct {
 	Index int
 }
 
-func solve(lines []string) int {
+// solvePt2 solves part 2 of the puzzle
+// It calculates the total voltage based on the 12 levels in each line
+func solvePt2(lines []string) int {
+	joltage := 0
+	for _, line := range lines {
+		levels := make([]int, len(line))
+		for i, char := range line {
+			l, _ := strconv.Atoi(string(char))
+			levels[i] = l
+		}
+		nLevels := len(levels)
+		desiredLevels := 12
+		// drop the n lowest levels
+		for _ = range nLevels - desiredLevels {
+			minIndex := 0
+			for i, level := range levels {
+				// if we reached the end drop the least significant digit
+				if i == len(levels)-1 {
+					minIndex = i
+					break
+				}
+				// we are travesing from most significant to least significant digit
+				// drop the first level that is lower than the next one
+				if level < levels[i+1] {
+					minIndex = i
+					break
+				}
+			}
+			// remove level at minIndex
+			levels = append(levels[:minIndex], levels[minIndex+1:]...)
+			// fmt.Println("removed ", minLevel, " levels now ", levels)
+		}
+		levelJoltage := 0
+		for i := range desiredLevels {
+			levelJoltage += levels[i] * int(math.Pow(10, float64((desiredLevels-1-i))))
+		}
+		joltage += levelJoltage
+		// fmt.Println("line ", line, "total joltage ", joltage, " joltage ", levelJoltage)
+	}
+	return joltage
+}
+
+// solvePt1 solves part 1 of the puzzle
+// It calculates the total voltage based on the two highest levels in each line
+func solvePt1(lines []string) int {
 	voltage := 0
 	for _, line := range lines {
 		levels := make([]Node, len(line))
