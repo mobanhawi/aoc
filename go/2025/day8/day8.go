793fe1a30d868254e79439ac7a3c527340082817
Elbanhawi, Mohamed <mohamed.elbanhawi@anz.com>
2025-12-08 17:08:29 +1100

    wip day8

---
+++ go/2025/day8/day8.go
@@ -0,0 +1,23 @@
+package day8
+
+import (
+	"fmt"
+
+	"github.com/mobanhawi/aoc/2025/util"
+)
+
+/*
+Solve https://adventofcode.com/2025/day/6
+*/
+func Solve() {
+	fmt.Println("2025/day/7 pt1", solvePt1(util.ReadLines("./2025/day7/input.txt")))
+	fmt.Println("2025/day/7 pt2", solvePt2(util.ReadLines("./2025/day7/input.txt")))
+}
+
+func solvePt2(lines []string) int {
+	return 0
+}
+
+func solvePt1(lines []string) int {
+	return 0
+}
